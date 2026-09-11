e43c01863be85de7f49500713b06c1824be41fba
Gabriel Alexandro Kaligis <2581026@unai.edu>
2026-09-11 09:33:57 +0700

    Perkenalan yang belum lengkap

---
+++ src/Perkenalan.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; // import Scanner
+
+public class Perkenalan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama Anda: ");
+        String nama = scanner.nextLine();
+
+        System.out.print("Masukkan NIM Anda: ");
+         String NIM = scanner.nextLine();
+
+        System.out.print("Masukkan umur Anda: ");
+        int umur = scanner.nextInt();
+
+
+
+        System.out.println("Halo " + nama + ", NIM " + NIM + ", umur " + umur + " tahun.");
+    }
+    }
